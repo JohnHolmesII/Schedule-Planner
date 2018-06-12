42a8f026e94c2b2ff8087ef00c4f559a94c832a4
JohnHolmesII <jedistephen@hotmail.com>
2018-06-11 22:37:53 -0700

    Custom linked list implementation

---
+++ Schedule Planner/NakedList.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedule_Planner
+{
+    class NakedList
+    {
+        public  uint Size { get; private set; }
+        private Node Head;
+
+        public NakedList()
+        {
+            Size = 0;
+            Head = null;
+        }
+
+        public void   Add(Object o)
+        {
+            if (o == null) return;
+
+            Node tmp = new Node(o);
+
+            if (Head == null)
+            {
+                Head = tmp;
+            }
+            else
+            {
+                tmp.Next  = Head.Next;
+                Head.Next = tmp;
+            }
+
+            ++Size;
+        }
+
+        public Object Get(uint index)
+        {
+            if (index > Size) return null;
+
+            Node tmp = Head;
+
+            for (int i = 0; i < index; ++i)
+            {
+                tmp = tmp.Next;
+            }
+
+            return tmp.Data;
+        }
+
+        public void   Remove(uint index)
+        {
+            if (index > Size) return;
+
+            if (index == 0)
+            {
+                Head = Head.Next;
+            }
+            else
+            {
+                Node prev = Head;
+                Node curr;
+
+                for (int i = 0; i < index - 1; ++1)
+                {
+                    prev = prev.Next;
+                }
+
+                curr      = prev.Next;
+                prev.Next = curr.Next;
+            }
+
+            --Size;
+        }
+
+        public void Flush()
+        {
+            Head = null;
+        }
+
+        private class Node
+        {
+            public Object Data { get; set; }
+            public Node   Next { get; set; }
+
+            public Node(Object d)
+            {
+                Data = d;
+                Next = null;
+            }
+        }
+    }
+}
