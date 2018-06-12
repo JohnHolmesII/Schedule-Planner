452cdaeb3785a88c912c04530921a0707b952218
JohnHolmesII <jedistephen@hotmail.com>
2018-06-11 20:09:33 -0700

    Begin database handler

---
+++ Schedule Planner/CourseDB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Schedule_Planner
+{
+    class CourseDB : ISerializable
+    {
+        public ICollection<string> Keys => throw new NotImplementedException();
+
+        public ICollection<Course> Values => throw new NotImplementedException();
+
+        public int Count => throw new NotImplementedException();
+
+        public void Add(string key, Course value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsKey(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
